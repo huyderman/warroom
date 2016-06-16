cd13e08fbd94f8ba4290f56ee550e45a32650bcb
Jo-Herman Haugholt <jo-herman@sonans.no>
2016-06-16 11:27:21 +0200

    replace `terminal-table` with `tty-table`

--- lib/war_room/die_table.rb
+++ lib/war_room/die_table.rb
@@ -1,5 +1,4 @@
 require 'dry-types'
-require 'terminal-table'
 require 'war_room/types'
 
 module WarRoom
@@ -7,28 +6,14 @@ module WarRoom
     class DieTableRow < Dry::Types::Struct
       attribute :range, Range
       attribute :result, Types::Coercible::String
+
+      def to_a
+        [range, result]
+      end
     end
 
     attribute :die,      Types::Coercible::String
     attribute :rows,     Types::Coercible::Array.member(DieTableRow)
     attribute :metadata, Types::Hash
-
-    def to_s
-      table_rows = rows.map do |table_row|
-        [die_range_string(table_row.range), table_row.result]
-      end
-
-      Terminal::Table.new(headings: [die, 'Result'], rows: table_rows).to_s
-    end
-
-    private
-
-    def die_range_string(range)
-      if range.begin == range.end
-        range.begin
-      else
-        "#{range.begin}–#{range.end}"
-      end
-    end
   end
 end
--- warroom.gemspec
+++ warroom.gemspec
@@ -18,7 +18,7 @@ Gem::Specification.new do |spec|
 
   spec.add_dependency 'gli', '~> 2.3'
   spec.add_dependency 'dry-types', '~> 0.7.0'
-  spec.add_dependency 'terminal-table', '~> 1.4'
+  spec.add_dependency 'tty-table', '~> 0.5.0'
 
   spec.add_development_dependency 'bundler', '~> 1.10'
   spec.add_development_dependency 'rake', '~> 10.0'
